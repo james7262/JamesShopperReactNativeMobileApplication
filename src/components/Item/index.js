c7f79fc4ee517d22718c78676aae133a4325b9e5
James Bachrach <bachrachj@chc.edu>
2022-10-24 10:11:00 -0400

    October 24th

---
+++ src/components/Item/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import styles from './styles';
+
+const Item = props => {
+    const post = props.post;
+    const onPress = () => {
+        console.log(post.name);
+    }
+
+  return (
+    <View style={styles.container}>
+        <TouchableOpacity style = {styles.touchable} onPress = {onPress}>
+            <View style = {{flex: 2}}>
+                <Text style = {styles.name} numberOfLines = {1}>
+                    {post.name}
+                </Text>
+                <Text style = {styles.price} numberOfLines = {1}>
+                    {post.price}
+                </Text>
+            </View>
+            <View style = {{flex: 1}}>
+                <Text style = {styles.quantity}>
+                    {post.quantity}
+                </Text>
+            </View>
+        </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Item;
--- src/screens/Items/index.js
+++ src/screens/Items/index.js
@@ -1,15 +1,65 @@
-import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, TouchableOpacity, Text, FlatList } from 'react-native';
+import Item from '../../components/Item';
+import { useNavigation } from '@react-navigation/native';
 import styles from './styles';
+import { openDatabase } from "react-native-sqlite-storage";
+
+const shopperDB = openDatabase({name: 'Shopper.db'});
+const itemsTableName = 'items';
 
 const ItemsScreen = props => {
 
+  const navigation = useNavigation();
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const listener = navigation.addListener('focus', () => {
+      let results = [];
+      shopperDB.transaction(txn => {
+        txn.executeSql(
+          `SELECT * FROM ${itemsTableName}`,
+          [],
+          (_, res) => {
+            let len = res.rows.length;
+            console.log('Length of items ' + len);
+            if (len > 0) {
+              for (let i = 0; i < len; i++) {
+                let item = res.rows.item(i);
+                results.push({
+                  id: item.id,
+                  name: item.name,
+                  price: item.price,
+                  quantity: item.quantity,
+                });
+              }
+              setItems(results);
+            } else {
+              setItems([]);
+            }
+          },
+          error => {
+            console.log('Error getting items ' + error.message);
+          },
+        )
+      });
+    });
+    return listener;
+  });
+
   return (
     <View style={styles.container}>
+      <View>
+        <FlatList 
+          data={items}
+          renderItem={({item}) => <Item post={item} />}
+          keyExtractor = {item => item.id}
+        />
+      </View>
         <View style={styles.bottom}>
             <TouchableOpacity 
                 style={styles.button}
-                onPress={() => console.log('Add Item!')}
+                onPress={() => navigation.navigate('Add Item')}
                 >
                 <Text style={styles.buttonText}>Add Item</Text>
             </TouchableOpacity>
